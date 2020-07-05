84aba4e1f6df37fa791d4b8541f9a467f1de12ad
Are <areifeoluwa10@gmail.com>
2020-07-05 19:12:10 +0100

    chore: added image_list view

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -3,9 +3,11 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import ImageCreateForm
 from .models import Image
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.views.decorators.http import require_POST
 from common.decorators import ajax_required
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+
 
 
 @login_required
@@ -54,3 +56,27 @@ def image_like(request):
         except:
             pass
         return JsonResponse({'status':'error'})
+
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        #If page is not an integer deliver th efirst page
+        images = paginator.page(1)
+    except EmptyPage:
+        if request.is_ajax():
+            return HttpResponse('')
+        images = paginator.page(paginator.num_pages)
+    if request.is_ajax():
+        return render(request,
+        'images/image/list_ajax.html',
+        {'section':'images', 'images':images})
+    render(request, 
+    'images/image/list.html',
+    {'section':'images', 'images':images})
+        
