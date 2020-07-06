21303ad64a3e123ce7fb306b7d2967e37539fd39
Are <areifeoluwa10@gmail.com>
2020-07-06 16:37:07 +0100

    migrations Contact from account

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -31,13 +31,13 @@ def image_create(request):
         form = ImageCreateForm(data=request.GET)
 
     return render(request, 'images/image/create.html',
-    {'section':'images', 'form':form})
+        {'section':'images', 'form':form})
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     return render(request,
-    'images/image/detail.html',
-    {'section':'images', 'image':image})
+        'images/image/detail.html',
+        {'section':'images', 'image':image})
 
 @ajax_required
 @login_required
@@ -74,9 +74,9 @@ def image_list(request):
         images = paginator.page(paginator.num_pages)
     if request.is_ajax():
         return render(request,
-        'images/image/list_ajax.html',
-        {'section':'images', 'images':images})
+            'images/image/list_ajax.html',
+            {'section':'images', 'images':images})
     return render(request, 
-    'images/image/list.html',
-    {'section':'images', 'images':images})
+        'images/image/list.html',
+        {'section':'images', 'images':images})
         
