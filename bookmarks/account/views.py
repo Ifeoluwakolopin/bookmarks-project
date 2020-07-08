1e3a566637fb39a2b3b30b358c6ec7c85e71c2c9
Are <areifeoluwa10@gmail.com>
2020-07-08 01:39:42 +0100

    feat: added util to create Actions easily. -  function also checks for repeated actions.
    -added user actions to activity stream

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -9,6 +9,7 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.views.decorators.http import require_POST
 from common.decorators import ajax_required
+from actions.utils import create_action
 
 
 def user_login(request):
@@ -50,6 +51,7 @@ def register(request):
                 user_form.cleaned_data['password'])
             new_user.save()
             Profile.objects.create(user=new_user)
+            create_action(new_user, 'has created an account')
             return render(request, 'account/register_done.html', {'new_user':new_user})
     else:
         user_form = UserRegistrationForm()
@@ -106,6 +108,7 @@ def user_follow(request):
                 Contact.objects.get_or_create(
                     user_from=request.user, user_to=user
                 )
+                create_action(request.user, 'is following', user)
             else:
                 Contact.objects.filter(user_from=request.user,
                     user_to=user).delete()
