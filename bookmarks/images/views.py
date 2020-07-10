feae886ba3814dfab9b178e66878cb14ef745d9f
Are <areifeoluwa10@gmail.com>
2020-07-10 01:29:23 +0100

    feat: integrated 'redis' to store item counts. - Installed redis. -Making use of redis to store the number of image views. -Redis inplace of Django ORM to  avoid an SQL update query for each view

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -8,6 +8,8 @@ from django.views.decorators.http import require_POST
 from common.decorators import ajax_required
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from actions.utils import create_action
+import redis
+from django.conf import settings
 
 
 @login_required
@@ -36,9 +38,13 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+    #Increment total image views by 1
+    total_views = r.incr(f'image:{image.id}:views')
     return render(request,
         'images/image/detail.html',
-        {'section':'images', 'image':image})
+        {'section':'images',
+         'image':image,
+         'total_views':total_views})
 
 @ajax_required
 @login_required
@@ -81,4 +87,8 @@ def image_list(request):
     return render(request, 
         'images/image/list.html',
         {'section':'images', 'images':images})
-        
+
+# CONNECT TO REDIS
+r = redis.Redis(host=settings.REDIS_HOST,
+    port=settings.REDIS_PORT,
+    db=settings.REDIS_DB)
