afd24e159be21a835a37849253f2284f0562d178
Ifeoluwakolopin <areifeoluwa10@gmail.com>
2020-10-07 12:10:38 +0100

    Revert "removed redundant login view"

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-
+    #path('login/', views.user_login, name='login'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'),
@@ -17,12 +17,16 @@ urlpatterns = [
     path('users/follow/', views.user_follow, name='user_follow'),
     path('users/', views.user_list, name='user_list'),
     path('users/<username>/', views.user_detail, name='user_detail'),
-    path('password_reset/', auth_views.PasswordResetView.as_view()),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(),
-        name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
-        name='password_reset_confirm'),
-    path('reset/done/',
-        auth_views.PasswordResetCompleteView.as_view(),
-        name='password_reset_complete')
+    
+
+    
+    #path('password_reset/', auth_views.PasswordResetView.as_view(),
+    #path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(),
+    #name='password_reset_done'),
+    #path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
+    #name='password_reset_confirm'),
+    #path('reset/done/',
+    #auth_views.PasswordResetCompleteView.as_view(),
+    #name='password_reset_complete'),
+
 ]
